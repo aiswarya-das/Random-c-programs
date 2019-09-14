fab91486a41abdf9fad9f3e231be361a515ebf1b
aiswarya <aiswaryadas.a@gmail.com>
2019-09-14 12:48:01 +0530

    Add oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,44 @@
+
+/* To calculate whether a given number is odd or even */
+
+#include<stdio.h>
+int main () {
+    int num ;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    if (num % 2 ==0)
+    {
+        printf("The given number is even\n");
+    }
+    else
+    {
+        printf("The given number is odd\n");
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Output :
+Enter a number : 5
+Number is odd */
+
+
+
